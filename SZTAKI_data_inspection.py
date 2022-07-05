09e43d8644f84f94d8b08d717a97d8099000a3ee
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2022-07-05 16:54:03 +0200

    több road sensor
    fél méterenként road sensor;
    ezek DevDistjéhez teszt;
    PathXY aktuális pontja az, ahová a szenzor néz: preview point

--- SZTAKI_data_inspection.py
+++ SZTAKI_data_inspection.py
@@ -29,14 +29,89 @@ RouteY_cc = mat_cc["RouteY"]
 PathX_cc = mat_cc["PathX"]
 PathY_cc = mat_cc["PathY"]
 
-DevDist00_cc = mat_cc["DevDist00"]
-DevAng00_cc = mat_cc["DevAng00"]
-DevDist05_cc = mat_cc["DevDist05"]
-DevAng05_cc = mat_cc["DevAng05"]
-DevDist10_cc = mat_cc["DevDist10"]
-DevAng10_cc = mat_cc["DevAng10"]
-DevDist15_cc = mat_cc["DevDist15"]
-DevAng15_cc = mat_cc["DevAng15"]
+PathX1_nc = mat_nc["PathX1"]
+PathY1_nc = mat_nc["PathY1"]
+
+DevDist000_nc = mat_nc["DevDist000"]
+DevAng000_nc = mat_nc["DevAng000"]
+DevDist005_nc = mat_nc["DevDist005"]
+DevAng005_nc = mat_nc["DevAng005"]
+DevDist010_nc = mat_nc["DevDist010"]
+DevAng010_nc = mat_nc["DevAng010"]
+DevDist015_nc = mat_nc["DevDist015"]
+DevAng015_nc = mat_nc["DevAng015"]
+DevDist020_nc = mat_nc["DevDist020"]
+DevAng020_nc = mat_nc["DevAng020"]
+DevDist025_nc = mat_nc["DevDist025"]
+DevAng025_nc = mat_nc["DevAng025"]
+DevDist030_nc = mat_nc["DevDist030"]
+DevAng030_nc = mat_nc["DevAng030"]
+DevDist035_nc = mat_nc["DevDist035"]
+DevAng035_nc = mat_nc["DevAng035"]
+DevDist040_nc = mat_nc["DevDist040"]
+DevAng040_nc = mat_nc["DevAng040"]
+DevDist045_nc = mat_nc["DevDist045"]
+DevAng045_nc = mat_nc["DevAng045"]
+DevDist050_nc = mat_nc["DevDist050"]
+DevAng050_nc = mat_nc["DevAng050"]
+DevDist055_nc = mat_nc["DevDist055"]
+DevAng055_nc = mat_nc["DevAng055"]
+DevDist060_nc = mat_nc["DevDist050"]
+DevAng060_nc = mat_nc["DevAng060"]
+DevDist065_nc = mat_nc["DevDist065"]
+DevAng065_nc = mat_nc["DevAng065"]
+DevDist070_nc = mat_nc["DevDist070"]
+DevAng070_nc = mat_nc["DevAng070"]
+DevDist075_nc = mat_nc["DevDist075"]
+DevAng075_nc = mat_nc["DevAng075"]
+DevDist080_nc = mat_nc["DevDist080"]
+DevAng080_nc = mat_nc["DevAng080"]
+DevDist085_nc = mat_nc["DevDist085"]
+DevAng085_nc = mat_nc["DevAng085"]
+DevDist090_nc = mat_nc["DevDist090"]
+DevAng090_nc = mat_nc["DevAng090"]
+DevDist095_nc = mat_nc["DevDist095"]
+DevAng095_nc = mat_nc["DevAng095"]
+DevDist100_nc = mat_nc["DevDist100"]
+DevAng100_nc = mat_nc["DevAng100"]
+DevDist105_nc = mat_nc["DevDist105"]
+DevAng105_nc = mat_nc["DevAng105"]
+DevDist110_nc = mat_nc["DevDist110"]
+DevAng110_nc = mat_nc["DevAng110"]
+DevDist115_nc = mat_nc["DevDist115"]
+DevAng115_nc = mat_nc["DevAng115"]
+DevDist120_nc = mat_nc["DevDist120"]
+DevAng120_nc = mat_nc["DevAng120"]
+DevDist125_nc = mat_nc["DevDist125"]
+DevAng125_nc = mat_nc["DevAng125"]
+DevDist130_nc = mat_nc["DevDist130"]
+DevAng130_nc = mat_nc["DevAng130"]
+DevDist135_nc = mat_nc["DevDist135"]
+DevAng135_nc = mat_nc["DevAng135"]
+DevDist140_nc = mat_nc["DevDist140"]
+DevAng140_nc = mat_nc["DevAng140"]
+DevDist145_nc = mat_nc["DevDist145"]
+DevAng145_nc = mat_nc["DevAng145"]
+DevDist150_nc = mat_nc["DevDist150"]
+DevAng150_nc = mat_nc["DevAng150"]
+DevDist155_nc = mat_nc["DevDist155"]
+DevAng155_nc = mat_nc["DevAng155"]
+DevDist160_nc = mat_nc["DevDist150"]
+DevAng160_nc = mat_nc["DevAng160"]
+DevDist165_nc = mat_nc["DevDist165"]
+DevAng165_nc = mat_nc["DevAng165"]
+DevDist170_nc = mat_nc["DevDist170"]
+DevAng170_nc = mat_nc["DevAng170"]
+DevDist175_nc = mat_nc["DevDist175"]
+DevAng175_nc = mat_nc["DevAng175"]
+DevDist180_nc = mat_nc["DevDist180"]
+DevAng180_nc = mat_nc["DevAng180"]
+DevDist185_nc = mat_nc["DevDist185"]
+DevAng185_nc = mat_nc["DevAng185"]
+DevDist190_nc = mat_nc["DevDist190"]
+DevAng190_nc = mat_nc["DevAng190"]
+DevDist195_nc = mat_nc["DevDist195"]
+DevAng195_nc = mat_nc["DevAng195"]
 
 
 # plt.figure(1)
@@ -59,12 +134,12 @@ DevAng15_cc = mat_cc["DevAng15"]
 # plt.plot(DevDist15_cc, 'b')
 # plt.show()
 
-plt.figure(5)
-plt.title("DevDist00 05 10 15 cc")
-plt.plot(DevDist00_cc, 'blue', label='00')
-plt.plot(DevDist05_cc, 'red', label='05')
-plt.plot(DevDist10_cc, 'orange', label='10')
-plt.plot(DevDist15_cc, 'green', label='15')
+plt.figure(6)
+plt.title("DevDist000 005 010 015 cc")
+plt.plot(DevDist000_nc, 'blue', label='00')
+plt.plot(DevDist005_nc, 'red', label='05')
+plt.plot(DevDist010_nc, 'orange', label='10')
+plt.plot(DevDist015_nc, 'green', label='15')
 plt.legend(loc=1)
 plt.show()
 
@@ -99,10 +174,10 @@ plt.show()
 
 plt.figure(100)
 plt.title("DevAng00 05 10 15 + SW cc")
-plt.plot(DevAng00_cc, 'blue', label='00')
-plt.plot(DevAng05_cc, 'red', label='05')
-plt.plot(DevAng10_cc, 'orange', label='10')
-plt.plot(DevAng15_cc, 'green', label='15')
+plt.plot(DevAng000_cc, 'blue', label='00')
+plt.plot(DevAng005_cc, 'red', label='05')
+plt.plot(DevAng010_cc, 'orange', label='10')
+plt.plot(DevAng015_cc, 'green', label='15')
 plt.plot(SW_cc, 'black', label='SW')
 plt.legend(loc=1)
 plt.show()
@@ -183,6 +258,31 @@ plt.show()
 # plt.title("path nc")
 # plt.plot(PathX_nc, PathY_nc, 'b')
 # plt.show()
+
+plt.figure(24)
+plt.title("path 0, 1")
+plt.plot(PathX_nc, PathY_nc, 'blue')
+plt.plot(PathX1_nc, PathY1_nc, 'red')
+plt.show()
+
+plt.figure(25)
+s = 10
+v = float(Vel[300])
+t = int(s / v * 1000)
+print(t)
+plt.plot(PathX_nc[300 : 300 + 3600], PathY_nc[300 : 300 + 3600], "blue", label="path")
+plt.plot(Pos_X_nc[300], Pos_Y_nc[300], "blue", marker='o', label="pos")
+x, y = rotate((Pos_X_nc[300], Pos_Y_nc[300]), Ori[300])
+plt.plot(x, y, "lightblue", marker='o', label="sens")
+xw, yw = rotate((x, y), Ori[300] + WheAng_nc[300])
+plt.plot(xw, yw, "black", marker='o', label="whe ang")
+xp, yp = PathX_nc[300 + t], PathY_nc[300 + t]
+plt.plot(xp, yp, "brown", marker='o', label="prev p")
+xp1, yp1 = PathX1_nc[300], PathY1_nc[300]
+plt.plot(xp1, yp1, "green", marker='o', label="prev p1")
+plt.axis('equal')
+plt.legend(loc=(1.04, 1))
+plt.show()
 
 # plt.figure(25)
 # plt.title("all nc")
