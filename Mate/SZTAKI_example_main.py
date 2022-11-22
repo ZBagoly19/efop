27d2f09c73f772e4d3ecb9ac0f9ac6a6e2fa66e5
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2022-11-22 09:27:39 +0100

    train/test vector, default net, net_name
    train/test vector-okkal adjuk meg, melyik adatokon tanítunk és tesztelünk
    default net: így már lesz egy alap háló, ha nem kérjük le a változókat a consts file-ból, akkor ezt tanítja
    a lekérés a get_variables_from_constsfile( ) függvénnyel történik
    net_name megadja, hogy melyik constructor csinálja a tanított hálót

--- Mate/SZTAKI_example_main.py
+++ Mate/SZTAKI_example_main.py
@@ -1,11 +1,22 @@
 import SZTAKI_constants_ as consts
-consts.net_type = "fully connected"      # "fully connected" or "convolutional"
+import SZTAKI_generic_net_creation_and_train as net_train
+trainClass = net_train.trainClass()
+
+
+consts.net_type = "convolutional"      # "fully connected" or "convolutional"
+consts.net_name = "cnn1"
+
 consts.optim_type = "adam"             # "adam"
+consts.weight_decay_ = 0.0
 
 consts.NUM_OF_INPUT_DATA_TYPES_1st_dim = 10
 consts.NUM_OF_INPUT_DATA_TYPES_2st_dim = 10
 consts.NUM_OF_INPUT_DATA_TYPES = 100
 consts.NUM_OF_OUTPUT_DATA_TYPES = 10
+
+consts.train_vector = [0, 1, 2]
+consts.test_vector = range(3, 5, 2)
+
 consts.EPOCHS = 1500
 consts.SAVE_EVERY_EPOCH = 1500
 
@@ -20,20 +31,29 @@ consts.width = 10
 
 consts.FILE_PRE = 'Mate_'
 
-import SZTAKI_generic_net_creation_and_train as net_train
-trainClass = net_train.trainClass()
 
+
+# consts file nelkul
+trainClass.setup()
+
+my_nets0 = trainClass.train()
+
+
+# consts file-al
+trainClass.get_variables_from_constsfile()
 trainClass.setup()
 
 my_nets1 = trainClass.train()
-print(my_nets1[0])
 
 
-
+# consts file-al, mas valtozokkal
 consts.net_type = "convolutional"
+consts.net_name = "cnn1"
 consts.learn_rate = 0.01
 consts.EPOCHS = 2000
+consts.SAVE_EVERY_EPOCH = 2000
+# trainClass.get_variables_from_constsfile()
 trainClass.setup()
 
 my_nets2 = trainClass.train()
-print(my_nets2[0])
+
