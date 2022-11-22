61131ed6c398f5f2cd77757a5cfcc09fd74ccd58
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2022-11-22 09:30:25 +0100

    train/test vector, default net, net_name
    train/test vector-okkal adjuk meg, melyik adatokon tanítunk és tesztelünk
    default net: így már lesz egy alap háló, ha nem kérjük le a változókat a consts file-ból, akkor ezt tanítja
    a lekérés a get_variables_from_constsfile( ) függvénnyel történik
    net_name megadja, hogy melyik constructor csinálja a tanított hálót

--- Mate/SZTAKI_constants_.py
+++ Mate/SZTAKI_constants_.py
@@ -1,10 +1,15 @@
 net_type = "convolutional"      # "fully connected" or "convolutional"
+net_name = "fc1"                # "fc1" or "cnn1"
 optim_type = "adam"             # "adam"
 
 NUM_OF_INPUT_DATA_TYPES_1st_dim = 1#0
 NUM_OF_INPUT_DATA_TYPES_2st_dim = 1#0
 NUM_OF_INPUT_DATA_TYPES = 1#00
 NUM_OF_OUTPUT_DATA_TYPES = 1#0
+
+train_vector = [0]
+test_vector = range(0, 4, 2)
+
 EPOCHS = 1#500
 SAVE_EVERY_EPOCH = 1#00
 
@@ -12,9 +17,9 @@ LearnRateDropPeriod = 1#50
 LearnRateDropFactor = 0.1#0.7
 
 learn_rate = 0.001
-# weight_decay = 0.00005
+weight_decay_ = 0.0
 
 num_of_layers = 0
 width = 1#0
 
-FILE_PRE = 'defaoul' # 'Mate_'
+FILE_PRE = 'default' # 'Mate_'
