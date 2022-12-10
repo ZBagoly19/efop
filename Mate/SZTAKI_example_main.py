145c5fa2842cb3bc90079e21f883e84ecaa89736
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2022-12-10 02:18:31 +0100

    settings, consts_ már nem kell
    tanító beállítások a settings-ben

--- Mate/SZTAKI_example_main.py
+++ Mate/SZTAKI_example_main.py
@@ -1,59 +1,46 @@
-import SZTAKI_constants_ as consts
 import SZTAKI_generic_net_creation_and_train as net_train
 trainClass = net_train.trainClass()
 
 
-consts.net_type = "convolutional"      # "fully connected" or "convolutional"
-consts.net_name = "cnn1"
-
-consts.optim_type = "adam"             # "adam"
-consts.weight_decay_ = 0.0
-
-consts.NUM_OF_INPUT_DATA_TYPES_1st_dim = 10
-consts.NUM_OF_INPUT_DATA_TYPES_2st_dim = 10
-consts.NUM_OF_INPUT_DATA_TYPES = 100
-consts.NUM_OF_OUTPUT_DATA_TYPES = 10
-
-consts.train_vector = [0, 1, 2]
-consts.test_vector = range(3, 5, 2)
-
-consts.EPOCHS = 1500
-consts.SAVE_EVERY_EPOCH = 1500
-
-consts.LearnRateDropFactor = 0.7
-consts.LearnRateDropPeriod = 150
-
-consts.learn_rate = 0.1
-# weight_decay = 0.00005
-
-consts.num_of_layers = 0
-consts.width = 10
-
-consts.FILE_PRE = 'Mate_'
-
-
-
-# consts file nelkul
+# megadott valtozok nelkul a default lesz
 trainClass.setup()
-
 my_nets0 = trainClass.train()
 
 
-# consts file-al
-trainClass.get_variables_from_constsfile()
+net_train.settings.net_type = "fully connected"      # "fully connected" or "convolutional"
+net_train.settings.net_name = "fc2"                 # fc1, fc2, cnn1
+net_train.settings.optim_type = "adam"             # "adam"
+net_train.settings.weight_decay_ = 0.0
+net_train.settings.NUM_OF_INPUT_DATA_TYPES_1st_dim = 10
+net_train.settings.NUM_OF_INPUT_DATA_TYPES_2st_dim = 10
+net_train.settings.NUM_OF_INPUT_DATA_TYPES = 100
+net_train.settings.NUM_OF_OUTPUT_DATA_TYPES = 10
+net_train.settings.train_vector = range(1, 100, 2)
+net_train.settings.test_vector = range(2, 100, 2)
+net_train.settings.EPOCHS = 100
+net_train.settings.SAVE_EVERY_EPOCH = 100
+net_train.settings.LearnRateDropFactor = 0.7
+net_train.settings.LearnRateDropPeriod = 150
+net_train.settings.learn_rate = 0.01
+net_train.settings.weight_decay_ = 0.00005
+net_train.settings.num_of_layers = 8
+net_train.settings.width = 10
+net_train.settings.FILE_PRE = 'Mate_'
+net_train.settings.DATA_FILE = 'Mate_data_train_2d__.npy'
+
+
+# setuppal amit beallitottunk
 trainClass.setup()
 
 my_nets1 = trainClass.train()
 
 
-# consts file-al, mas valtozokkal
-consts.net_type = "convolutional"
-consts.net_name = "cnn1"
-consts.learn_rate = 0.01
-consts.EPOCHS = 2000
-consts.SAVE_EVERY_EPOCH = 2000
-# trainClass.get_variables_from_constsfile()
+# mas valtozokkal
+net_train.settings.net_type = "convolutional"
+net_train.settings.net_name = "cnn1"
+net_train.settings.learn_rate = 0.01
+net_train.settings.EPOCHS = 200
+net_train.settings.SAVE_EVERY_EPOCH = 200
 trainClass.setup()
 
 my_nets2 = trainClass.train()
-
