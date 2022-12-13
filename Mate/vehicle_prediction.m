b11ae90d6493cdd352224309937223828715ad86
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2022-12-13 14:30:44 +0100

    szépítgetés és befejezés
    ebben a formában 2 másodpercre előre becsli a lateral error-t, azt a 2. sorban lehet állítani

--- Mate/vehicle_prediction.m
+++ Mate/vehicle_prediction.m
@@ -1,28 +1,112 @@
-%% teszt adatok
-X = Pos_X(4000);
-Y = Pos_Y(4000);
-Ori = Ori_Z(4000);
-WA = WheAng_avg(4000);
-V = Vel(4000);
-Pos_X_rear = Pos_X_rearaxle(4000);
-Pos_Y_rear = Pos_Y_rearaxle(4000);
+%% prediction time
+tspan_end = 2;
 
-[front_x, front_y] = calculate_middle_of_front_axel(X, Y, Ori);
-wheel_base = get_wheelbase(front_x, front_y, Pos_X_rear, Pos_Y_rear);
+%% test data
+load('meas_cornercut0_speed50_acc333_longlong_sens.mat')
+chosen_point = 4000;
+X = Pos_X(chosen_point);
+Y = Pos_Y(chosen_point);
+Ori = Ori_Z(chosen_point);
+WA = WheAng_avg(chosen_point);
+V = Vel(chosen_point);
+Pos_X_rear = Pos_X_rearaxle(chosen_point);
+Pos_Y_rear = Pos_Y_rearaxle(chosen_point);
 
+[front_x, front_y] = rotate_and_translate(X, Y, Ori, 1.45);
+wheel_base = get_wheelbase(front_x, front_y, Pos_X_rear, Pos_Y_rear)
+
+
+%% prediction
+% this uses Robotics System Toolbox
 kinematicModel = bicycleKinematics('WheelBase', wheel_base)
 initialState = [Pos_X_rear Pos_Y_rear Ori];
-tspan = 0 : 0.01 : 1;  % from now (0), with timestep 1 sec (1), 1 sec ahead (1)
+tspan = 0 : 0.01 : tspan_end;  % from now : with timestep 0.01 sec : 1 sec ahead
 inputs = [V WA]; %Turn left
-[t,y] = ode45(@(t,y)derivative(kinematicModel, y, inputs),tspan,initialState);
+[t, y] = ode45(@(t, y)derivative(kinematicModel, y, inputs), tspan, initialState);
+pred_length = size(y);
+pred_rear_X = y(pred_length(1), 1);
+pred_rear_Y = y(pred_length(1), 2);
+pred_Ori = y(pred_length(1), 3);
+[predicted_front_x, predicted_front_y] = rotate_and_translate(pred_rear_X, pred_rear_Y, pred_Ori, wheel_base);
 
-figure(2)
-plot(y(:,1),y(:,2))
+% figure(1)
+% axis equal
+% hold on
+% %plot(PathX, PathY)
+% plot(PathX(chosen_point-100:chosen_point+300), PathY(chosen_point-100:chosen_point+300))
+% plot(y(:,1),y(:,2))
+% 
+% P0x = predicted_front_x
+% P0y = predicted_front_y
+% 
+% P1x = P0x + 1
+% P1y = P0y + tan(pred_Ori + pi/2)
+% plot([P0x, P1x], [P0y, P1y])
+% 
+% legend('path', ...
+%     'prediction of middle of rear axel', ...
+%     'perpendicular line to orientation from middle of front axel')
+
+
+%% intersection
+perpendicular_line_x = [P0x, P1x]
+perpendicular_line_y = [P0y, P1y]
+% this uses Fast and Robust Curve Intersections
+robust = 1;
+[x0, y0, iout, jout] = intersections(PathX, PathY, perpendicular_line_x, perpendicular_line_y, robust)
+
+% figure(2)
+% axis equal
+% hold on
+% %plot(PathX, PathY)
+% plot(PathX(chosen_point-100:chosen_point+300), PathY(chosen_point-100:chosen_point+300));
+% plot(y(:,1),y(:,2));
+% 
+% P0x = predicted_front_x;
+% P0y = predicted_front_y;
+% 
+% P1x = P0x + 1;
+% P1y = P0y + tan(pred_Ori + pi/2);
+% plot([P0x, P1x], [P0y, P1y]);
+% 
+% plot(x0, y0, 'o');
+% 
+% legend('path', ...
+%     'prediction of middle of rear axel', ...
+%     'perpendicular line to orientation from middle of front axel', ...
+%     'intersection')
+
+
+%% lateral distance
+figure(3)
+axis equal
+hold on
+%plot(PathX, PathY)
+plot(PathX(chosen_point-100:chosen_point+300), PathY(chosen_point-100:chosen_point+300));
+plot(y(:,1),y(:,2));
+
+P0x = predicted_front_x;
+P0y = predicted_front_y;
+
+P1x = P0x + 1;
+P1y = P0y + tan(pred_Ori + pi/2);
+plot([P0x, P1x], [P0y, P1y]);
+
+plot(x0, y0, 'o');
+plot(predicted_front_x, predicted_front_y, 'o');
+
+legend('path', ...
+    'prediction of middle of rear axel', ...
+    'perpendicular line to orientation from middle of front axel', ...
+    'intersection', ...
+    'middle of front axel')
+
+LATERAL_ERROR = sqrt((predicted_front_x-x0)*(predicted_front_x-x0) + (predicted_front_y-y0)*(predicted_front_y-y0))
 
 
 %% functions
-function [qx, qy] = calculate_middle_of_front_axel(ox, oy, angle)
-    px = ox + 1.45; % 1.45: az auto kozepe es az elso tengely tavolsaga
+function [qx, qy] = rotate_and_translate(ox, oy, angle, tra)
+    px = ox + tra;
     py = oy;
 
     qx = ox + cos(angle) * (px - ox) - sin(angle) * (py - oy);
@@ -33,5 +117,5 @@ function wheel_base = get_wheelbase(fx, fy, rx, ry)
     x = fx - rx;
     y = fy - ry;
 
-    wheel_base = sqrt(x*x + y*y)
+    wheel_base = sqrt(x*x + y*y);
 end
