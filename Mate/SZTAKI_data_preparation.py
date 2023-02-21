6583346e35a5ae9022aaca054a115e19d26d4b73
Bagoly Zoltán <47918052+ZBagoly19@users.noreply.github.com>
2023-02-21 19:01:43 +0100

    pusztán 1 file kreálása
    már nem kell külön test file, mert az össz adatból később választjuk ki, melyek a test adatpontok

--- Mate/SZTAKI_data_preparation.py
+++ Mate/SZTAKI_data_preparation.py
@@ -4,28 +4,20 @@ import numpy as np
 
 filename = "data_to_zoli.mat"
 mat = scipy.io.loadmat(filename)
-train_input = mat["data_train_input_1"]
-train_label = mat["data_train_output"]
+input_data = mat["data_train_input_1"]
+label_data = mat["data_train_output"]
 test_input = mat["data_test_input_1"]
 test_label = mat["data_test_output"]
 
 train_data = [None] * len(train_input[0][0])
-testing_data = [None] * len(test_input[0][0])
 
-for data_point_idx1 in range(len(train_input[0][0])):
-    train_data[data_point_idx1] = [
-        train_input[:, :, data_point_idx1].reshape(100, 1),
-        train_label[data_point_idx1],
-        data_point_idx1
-    ]
-for data_point_idx2 in range(len(test_input[0][0])):
-    testing_data[data_point_idx2] = [
-        test_input[:, :, data_point_idx2].reshape(100, 1),
-        test_label[data_point_idx2],
-        data_point_idx2
+for data_point_idx in range(len(input_data[0][0])):
+    train_data[data_point_idx] = [
+        input_data[:, :, data_point_idx].reshape(100, 1),
+        label_data[data_point_idx],
+        data_point_idx
     ]
 
-np.random.shuffle(testing_data)
-np.save("Mate_data_test_part__" + ".npy", testing_data)
+
 np.random.shuffle(train_data)
 np.save("Mate_data_train_part__" + ".npy", train_data)
